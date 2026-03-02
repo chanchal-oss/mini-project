79bf361c83c8093b0862b8c283131b21c0f94fb6
Chanchal-oss <chanchalsoni760@gmail.com>
2026-03-02 22:01:29 +0530

    dele

--- teacher.py
+++ teacher.py
@@ -1,16 +1,1 @@
-class Teacher:
-    def __init__(self,name,salary,sub):
-        self.name=name
-        self.salary=salary
-        self.sub=sub
-    def display(self):
-        print(self.name)
-        print(self.salary)
-        print(self.sub)
-    def greet(self):
-        print("ok thanking to you mam.....\n",self.name)
-t1=Teacher("j",98000,"PAI")
-t1.display()
-t1.greet()
-
 
